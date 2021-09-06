27e9267ba4d4149eff177096b64a1af80130c095
ashwith31 <ashwitash@gmail.com>
2021-09-06 20:56:01 +0530

    Modified:Done with Test case 2

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -10,4 +10,9 @@ public class MoodAnalyserTest {
         String mood = moodAnalyser.analyseMood("I am in sad mood.");
         Assertions.assertEquals("sad", mood);
     }
+    @Test
+    public void Given_I_am_in_Any_Mood_message_Should_Return_HAPPY(){
+        String mood = moodAnalyser.analyseMood("I am in any mood.");
+        Assertions.assertEquals("Happy", mood);
+    }
 }
