d8f74aa3fa774e2879656ae41959bc8fe65ee0f7
ashwith31 <ashwitash@gmail.com>
2021-09-06 20:38:36 +0530

    Modified:Created a mood analyser object

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -1,7 +1,11 @@
 package com.moodanalyser;
 
 public class MoodAnalyser {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Mood Analyser Program");
+
+    public static String analyseMood(String message) {
+        if (message.contains("sad"))
+            return "sad";
+        else
+            return "Happy";
     }
 }
