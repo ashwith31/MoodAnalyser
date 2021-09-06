5233e6cce896edf5d73e68f9a4a12239c02c854e
ashwith31 <ashwitash@gmail.com>
2021-09-06 20:44:09 +0530

    Modified:Done the test case 1

--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -5,4 +5,9 @@ import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
     MoodAnalyser moodAnalyser = new MoodAnalyser();
+    @Test
+    public void Given_I_am_in_Sad_Mood_message_Should_Return_SAD() {
+        String mood = moodAnalyser.analyseMood("I am in sad mood.");
+        Assertions.assertEquals("sad", mood);
+    }
 }
