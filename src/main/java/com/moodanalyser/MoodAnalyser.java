a1eb3bc41d1da1a2b8bdb578c848e476d322129b
ashwith31 <ashwitash@gmail.com>
2021-09-06 21:03:58 +0530

    Modified:Created a constructor

--- src/main/java/com/moodanalyser/MoodAnalyser.java
+++ src/main/java/com/moodanalyser/MoodAnalyser.java
@@ -2,10 +2,16 @@ package com.moodanalyser;
 
 public class MoodAnalyser {
 
+    private String message;
+
     public static String analyseMood(String message) {
         if (message.contains("sad"))
             return "sad";
         else
             return "Happy";
+    }
+
+    void MoodAnalyser(String message) {
+        this.message = message;
     }
 }
--- src/test/java/com/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodanalyser/MoodAnalyserTest.java
@@ -4,15 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyserTest {
-    MoodAnalyser moodAnalyser = new MoodAnalyser();
-    @Test
-    public void Given_I_am_in_Sad_Mood_message_Should_Return_SAD() {
-        String mood = moodAnalyser.analyseMood("I am in sad mood.");
-        Assertions.assertEquals("sad", mood);
-    }
-    @Test
-    public void Given_I_am_in_Any_Mood_message_Should_Return_HAPPY(){
-        String mood = moodAnalyser.analyseMood("I am in any mood.");
-        Assertions.assertEquals("Happy", mood);
-    }
+    MoodAnalyser moodAnalyser =null;
+
 }
